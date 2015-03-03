439c482572b29cf5832b1699d559d6540161af4b
Lau <laruku.cosmes@gmail.com>
2015-03-03 17:56:58 +0000

    Se modifico archivo 'index.php'. Instalacion de 'clickcoder', y modificacion de 'index.blade.php'

--- public/index.php
+++ public/index.php
@@ -3,7 +3,19 @@
   require("../vendor/autoload.php");
   
   #instance a new Slim aplication
-  $app = new \Slim\Slim();
+  $app = new \Slim\Slim (
+      array(
+          #adds application settings
+          'view' => new \Slim\Views\Blade(),
+          'templates.path' => '../templates',
+        )
+    );
+  #seput templates compiled cache
+  $view = $app->view();
+  $view ->parseOptions = array(
+      'debug'=>true,
+      'cache'=>'../html_cache'
+    );
   
   #defines a route for the GET method
   $app->get("/", function(){
