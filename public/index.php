8570a098ab97b3486f6f7b3bc8ae085d39ae25f3
Lau <laruku.cosmes@gmail.com>
2015-03-08 04:54:32 +0000

    Se crea archivo otro 'index.php' y se 'enlaza' al 'index2_1.html'. Se reinstala slim-blade

--- public/index.php
+++ public/index.php
@@ -1,27 +1,15 @@
 <?php
-  #Incluide the vendors autoload
-  require("../vendor/autoload.php");
-  
-  #instance a new Slim aplication
-  $app = new \Slim\Slim (
-      array(
-          #adds application settings
-          'view' => new \Slim\Views\Blade(),
-          'templates.path' => '../templates',
-        )
-    );
-  #seput templates compiled cache
-  $view = $app->view();
-  $view ->parseOptions = array(
-      'debug'=>true,
-      'cache'=>'../html_cache'
-    );
-  
-  #defines a route for the GET method
-  $app->get("/", function(){
-     $view=require("index.html");
-     echo $view;
-});
-
-  #Actually runs the application
-  $app->run();
+	#Incluide the vendors autoload
+	require("../vendor/autoload.php");
+	
+	#instance a new Slim aplication
+	$app = new \Slim\Slim();
+	
+	#defines a route for the GET method
+	$app->get("/", function(){
+		$view = require("index2_1.html");
+		echo $view;
+	});
+	
+	#Actually runs the application
+	$app->run();
